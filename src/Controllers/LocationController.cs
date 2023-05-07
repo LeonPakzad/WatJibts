06088fbb200f41dc02934e5eaa9cb4aeecb15e08
LeonPakzad <44465654+LeonPakzad@users.noreply.github.com>
2023-05-07 21:14:38 +0200

    added very important burger feature

---
+++ src/Controllers/LocationController.cs
@@ -0,0 +1,17 @@
+
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using src.Data;
+
+namespace src.Controllers;
+
+public class LocationController : Controller
+{
+    // private Location db = new WatDbContext();
+
+    // GET: /Movies/
+    // public ActionResult Index()
+    // {
+        // return View(db.Location.ToList());
+    // }
+}
