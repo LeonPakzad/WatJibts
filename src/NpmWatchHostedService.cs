feedc890f542ab7ac1fd8b39bcc73bcc20786f86
LeonPakzad <pakzadleon@gmail.com>
2023-07-10 21:37:45 +0200

    minor class name change

--- src/NpmWatchHostedService.cs
+++ src/NpmWatchHostedService.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace ExampleApplication
+namespace SassCheck
 {
 #if DEBUG
     internal sealed class NpmWatchHostedService : IHostedService, IDisposable
